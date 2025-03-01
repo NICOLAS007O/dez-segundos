5c96fa70f51b9ec1c7c485b34f2c207a9c92869c
nicolas souza <nicolas.nicotali@gmail.com>
2025-02-28 22:24:16 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("preparar..."); Console.ReadKey();
+Console.WriteLine("apontar..."); Console.ReadKey();
+Console.WriteLine("fogo..."); Console.ReadKey();
+Console.Clear();
+
+
+
+
+
